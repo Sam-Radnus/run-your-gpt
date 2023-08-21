bf3a45ba04d10ed3309c70069e99adf711389bad
Sam-Sundar <68651028+Sam-Radnus@users.noreply.github.com>
2023-08-21 19:45:16 +0530

    Update ChatSection.js

--- project/src/components/ChatSection.js
+++ project/src/components/ChatSection.js
@@ -24,8 +24,8 @@ const ChatSection = () => {
     const [promptsLeft,setPromptsLeft]=useState(25);
     const [disabled,setDisabled]=useState(false);
     const navigate=useNavigate();
-   
-
+    
+    const URI='wss://archived-immune-footage-estonia.trycloudflare.com/api/v1/stream'
  
     useEffect(() => {
      const data=JSON.parse(localStorage.getItem('userInfo'));
@@ -34,6 +34,120 @@ const ChatSection = () => {
      fetchInfo();
      setUserInfo(data);
     }, [])
+    const run = async (user_input, history) => {
+        const request = {
+            user_input: user_input,
+            prompt: user_input,
+            max_new_tokens: 250,
+            auto_max_new_tokens: false,
+            history: history,
+            mode: 'instruct',  // Valid options: 'chat', 'chat-instruct', 'instruct'
+            character: 'Example',
+            instruction_template: 'Vicuna-v1.1', // Will get autodetected if unset
+            your_name: 'You',
+             // 'name1': 'name of user', # Optional
+             // 'name2': 'name of character', # Optional
+             // 'context': 'character context', # Optional
+             // 'greeting': 'greeting', # Optional
+             // 'name1_instruct': 'You', # Optional
+             // 'name2_instruct': 'Assistant', # Optional
+             // 'context_instruct': 'context_instruct', # Optional
+             // 'turn_template': 'turn_template', # Optional
+            regenerate: false,
+            _continue: false,
+            chat_instruct_command: 'Continue the chat dialogue below. Write a single reply for the character "<|character|>".\n\n<|prompt|>',
+    
+        
+            do_sample: true,
+            preset: 'None',
+            top_p: 0.1,
+            temperature: 0.7,
+            epsilon_cutoff: 0,
+            typical_p: 1,
+            tfs: 1,
+            eta_cutoff: 0,  
+            repetition_penalty: 1.18,
+            top_a: 0,
+            top_k: 40,
+            repetition_penalty_range: 0,
+            no_repeat_ngram_size: 0,
+            min_length: 0,
+            penalty_alpha: 0,
+            num_beams: 1,
+            early_stopping: false,
+            length_penalty: 1,
+            mirostat_tau: 5,
+            mirostat_mode: 0,
+            guidance_scale: 1,
+            mirostat_eta: 0.1,
+            
+            negative_prompt: '',
+            add_bos_token: true,
+            seed: -1,
+            ban_eos_token: false,
+            truncation_length: 2048,
+            stopping_strings: [],
+            skip_special_tokens: true,
+        };
+      
+        return new Promise((resolve, reject) => {
+          const websocket = new WebSocket(URI);
+          let responseText = "";
+      
+          websocket.onopen = () => {
+            try {
+              websocket.send(JSON.stringify(request));
+            } catch (error) {
+              reject(`Failed to send data to WebSocket: ${error.message}`);
+            }
+          };
+      
+          websocket.onmessage = (event) => {
+            try {
+              const incomingData = JSON.parse(event.data);
+      
+              switch (incomingData.event) {
+                case 'text_stream':
+                  responseText += incomingData.text;
+                  break;
+                case 'stream_end':
+                  resolve(responseText);
+                  websocket.close();
+                  break;
+                default:
+                  reject(`Unexpected event type: ${incomingData.event}`);
+                  break;
+              }
+            } catch (error) {
+              reject(`Error processing WebSocket message: ${error.message}`);
+            }
+          };
+      
+          websocket.onerror = (error) => {
+            reject(`WebSocket Error: ${error.message}`);
+          };
+      
+          websocket.onclose = (event) => {
+            if (!event.wasClean) {
+              reject(`WebSocket connection closed unexpectedly: ${event.code} ${event.reason}`);
+            }
+          };
+        });
+      };
+      
+      const history = { internal: [], visible: [] };
+      
+      const printResponseStream = async (prompt) => {
+        try {
+          const responseText = await run(prompt, history);
+          history.internal.push(responseText);
+          return responseText;
+        } catch (error) {
+          console.error(`Error in printResponseStream: ${error.message}`);
+          return "An error occurred while processing your request.";
+        }
+      };
+      
     const fetchInfo=async(token)=>{
         try {
             const token =localStorage.getItem('token');
@@ -77,16 +191,15 @@ const ChatSection = () => {
         ]);
         setIsLoading(true);
         const data=await sendMessage(userInput);
-        if(data?.promptsLeft){
-           setPromptsLeft(data.promptsLeft);
+        if(data){
+           setPromptsLeft((prompts)=>{
+                return prompts-1;
+           });
         }
         setIsLoading(false);
         if(data){
-            fetchInfo();
-
-
            setMessages(prevMessages => [...prevMessages,
-               { type: 'bot', text: data.response, timestamp: data.current_time }
+               { type: 'bot', text: data, timestamp:currentTime }
            ]);
         }
         else{
@@ -99,16 +212,12 @@ const ChatSection = () => {
     const sendMessage = async (message) => {
         const token=localStorage.getItem('token');
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/chat/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization':`Bearer ${token}`
-                },
-                body: JSON.stringify({ prompt: message })
-            });
-            const data = await response.json();
-            return data;
+            if(token){
+               const data = await printResponseStream(message);
+               console.log(data);
+               return data;
+            }
+            else return null;
         } catch (error) {
             console.error("Error sending message:", error);
         }
